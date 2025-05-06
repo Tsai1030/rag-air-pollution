b59262ec348b2d9884b3ea75475f80f0b65c625d
Tsai1030 <pijh102511@gmail.com>
2025-05-06 10:25:42 +0800

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,20 @@
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: { extend: {} },
-  plugins: [],
-}
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./index.html",        // 依你的專案路徑調整
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          pumpkin: '#D06A42',
+          creamwhite: '#F9F8F3', // ← 
+          creamlight: '#F2EFE7',
+          milky: '#FCFBF9',
+          cc:'#D38A74'
+        },
+      },
+    },
+    plugins: [],
+  }
+  
